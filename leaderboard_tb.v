85b07c6b618cb7f01918ed15739660d30ccd8136
PureCdr289 <151774828+PureCdr289@users.noreply.github.com>
2023-12-13 18:42:34 -0500

    Update leaderboard_tb.v

--- leaderboard_tb.v
+++ leaderboard_tb.v
@@ -24,17 +24,22 @@ module leaderboard_tb(
 
     );
     
-    reg [21:0]time_in;
+    reg [38:0]time_in;
     reg [1:0] stopwatch_mode;
     reg [2:0] display_mode;
-    wire [21:0] leaderboard_number;
+    wire [38:0] fast_1;
+    wire [38:0] fast_2;
+    wire [38:0] fast_3;
+    wire [38:0] slow_1;
+    wire [38:0] slow_2;
+    wire [38:0] slow_3;
     wire signal_sound_1;
     wire signal_sound_2;
     wire signal_sound_3;
     wire [2:0] leaderboard_LED;
     wire [1:0] slow_or_fast;
     
-    leaderboard lb(.time_in(time_in),.stopwatch_mode(stopwatch_mode),.display_mode(display_mode),.leaderboard_number(leaderboard_number),.signal_sound_1(signal_sound_1),.signal_sound_2(signal_sound_2),.signal_sound_3(signal_sound_3),.leaderboard_LED(leaderboard_LED),.slow_or_fast(slow_or_fast));
+    leaderboard lb(.time_in(time_in),.stopwatch_mode(stopwatch_mode),.display_mode(display_mode),.fast_1(fast_1),.fast_2(fast_2),.fast_3(fast_3),.slow_1(slow_1),.slow_2(slow_2),.slow_3(slow_3),.signal_sound_1(signal_sound_1),.signal_sound_2(signal_sound_2),.signal_sound_3(signal_sound_3),.leaderboard_LED(leaderboard_LED),.slow_or_fast(slow_or_fast));
     
     
     initial begin
@@ -54,6 +59,6 @@ module leaderboard_tb(
     #10
     $finish;
     end
-    
+    //Simulation works as expected, signals for sound change when a change in input (time_in) occurs that causes a shift in rank in either mode.
 endmodule
 
